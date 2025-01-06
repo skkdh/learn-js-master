d1bf0def90b75c570853955206a96d700bdf15b2
skkdh <138329964+skkdh@users.noreply.github.com>
2025-01-06 18:42:55 +0200

    Create countDown.test.js

---
+++ src/countDown.test.js
@@ -0,0 +1,15 @@
+import {countdown} from './countdown';
+
+describe("tests for countdown function", () => {
+    it("should count from biggest number to smallest", () => {
+        expect(countdown(10)).toEqual([10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]);
+    });
+
+    it("should count from 5 to 0", () => {
+        expect(countdown(5)).toEqual([5, 4, 3, 2, 1, 0]);
+    });
+
+    it("should count from 0 to 0", () => {
+        expect(countdown(0)).toEqual([0]);
+    });
+});
